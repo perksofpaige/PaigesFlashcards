260e03996fa37de1b37937f5d0b8bcec00387407
perksofpaige <paigelee@ufl.edu>
2018-11-30 22:07:13 -0500

    Updates from Lab 4

--- app/src/main/java/com/example/paigelee/paigesflashcards/MainActivity.java
+++ app/src/main/java/com/example/paigelee/paigesflashcards/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.paigelee.paigesflashcards;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.plattysoft.leonids.ParticleSystem;
+
 import java.util.List;
 import java.util.Random;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -45,14 +57,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+              //  findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+              //  findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                View answerSideView = findViewById(R.id.flashcard_answer);
+                final View questionSideView= findViewById(R.id.flashcard_question);
+
+                answerSideView.setVisibility(View.VISIBLE);
+                questionSideView.setVisibility(View.INVISIBLE);
+
                 findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.Yellow));
                 findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.Yellow));
                 findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.Yellow));
+
+                // get the center for the clipping circle
+                int cx = answerSideView.getWidth() / 2;
+                int cy = answerSideView.getHeight() / 2;
+
+                // get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+                // create the animator for this view (the start radius is zero)
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
+
+                // hide the question and show the answer to prepare for playing the animation!
+
+
+                anim.setDuration(3000);
+                anim.start();
 
 
             }
@@ -61,11 +98,19 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            //    new ParticleSystem(MainActivity.this, 100, R.drawable.confetti, 3000)
+             //           .setSpeedRange(0.2f, 0.5f)
+             //           .oneShot(findViewById(R.id.answer3), 100);
+
+
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+
                 findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.colorPurpleBackground));
                 findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.colorPurpleBackground));
                 findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.colorPurpleBackground));
+
             }
 
         });
@@ -92,16 +137,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.RightGreen));
 
+                new ParticleSystem(MainActivity.this, 100, R.drawable.confetti, 3000)
+                        .setSpeedRange(0.2f, 0.5f)
+                        .oneShot(findViewById(R.id.answer3), 100);
+
 
             }
 
         });
 
-        findViewById(R.id.toggle_choices_invisibility).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.unseeIcon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.toggle_choices_invisibility).setVisibility(View.INVISIBLE);
-                findViewById(R.id.toggle_choices_visibility).setVisibility(View.VISIBLE);
+                findViewById(R.id.unseeIcon).setVisibility(View.INVISIBLE);
+                findViewById(R.id.seeIcon).setVisibility(View.VISIBLE);
                 findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
                 findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
                 findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
@@ -109,11 +158,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.seeIcon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.toggle_choices_invisibility).setVisibility(View.VISIBLE);
-                findViewById(R.id.toggle_choices_visibility).setVisibility(View.INVISIBLE);
+                findViewById(R.id.unseeIcon).setVisibility(View.VISIBLE);
+                findViewById(R.id.seeIcon).setVisibility(View.INVISIBLE);
                 findViewById(R.id.answer1).setVisibility(View.VISIBLE);
                 findViewById(R.id.answer2).setVisibility(View.VISIBLE);
                 findViewById(R.id.answer3).setVisibility(View.VISIBLE);
@@ -123,20 +172,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button addButton =
-                (Button) findViewById(R.id.addButton);
+                findViewById(R.id.addButton);
 
 
         addButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
 
         Button editButton =
-                (Button) findViewById(R.id.editButton);
+                findViewById(R.id.editButton);
 
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -165,12 +216,45 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.right_in);
+
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+
+
+                if(allFlashcards.size() == 0)  {
+                    return;
+                    // add toast message
+                    //Toast toast = Toast.makeText(getApplicationContext(), "There are no remaining cards.", Toast.LENGTH_SHORT);
+                  //  toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                  //  toast.show();
+                }
                 // advance our pointer index so we can show the next card
                 currentCardDisplayedIndex = getRandomNumber(0, allFlashcards.size() - 1);
-                ;
+
 
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).startAnimation(leftOutAnim);
+
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
 
 
                 // set the question and answer TextViews with data from the database
@@ -213,31 +297,36 @@ public class MainActivity extends AppCompatActivity {
 
         allFlashcards = flashcardDatabase.getAllCards();
 
-        String question = data.getExtras().getString("question");
-        String answer = data.getExtras().getString("answer");
-        String wrongAnswer1 = data.getExtras().getString("wrong_answer1");
-        String wrongAnswer2 = data.getExtras().getString("wrong_answer2");
+if (resultCode == RESULT_OK){
 
-        if (requestCode == 100 && resultCode == RESULT_OK) {
+    String question = data.getExtras().getString("question");
+    String answer = data.getExtras().getString("answer");
+    String wrongAnswer1 = data.getExtras().getString("wrong_answer1");
+    String wrongAnswer2 = data.getExtras().getString("wrong_answer2");
+
+    if (requestCode == 100) {
 
 
-            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
-            ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
+        ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+        ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
 
-            ((TextView) findViewById(R.id.answer1)).setText(wrongAnswer1);
-            ((TextView) findViewById(R.id.answer2)).setText(wrongAnswer2);
-            ((TextView) findViewById(R.id.answer3)).setText(answer);
+        ((TextView) findViewById(R.id.answer1)).setText(wrongAnswer1);
+        ((TextView) findViewById(R.id.answer2)).setText(wrongAnswer2);
+        ((TextView) findViewById(R.id.answer3)).setText(answer);
 
-            flashcardDatabase.insertCard(new Flashcard(question, answer, wrongAnswer1, wrongAnswer2));
-        }
-        else if (requestCode == EDIT_CARD_REQUEST_CODE && resultCode == RESULT_OK) {
-            cardToEdit.setQuestion(question);
-            cardToEdit.setAnswer(answer);
-            cardToEdit.setWrongAnswer1(wrongAnswer1);
-            cardToEdit.setWrongAnswer2(wrongAnswer2);
+        flashcardDatabase.insertCard(new Flashcard(question, answer, wrongAnswer1, wrongAnswer2));
+    }
+    else if (requestCode == EDIT_CARD_REQUEST_CODE) {
+        cardToEdit.setQuestion(question);
+        cardToEdit.setAnswer(answer);
+        cardToEdit.setWrongAnswer1(wrongAnswer1);
+        cardToEdit.setWrongAnswer2(wrongAnswer2);
 
-            flashcardDatabase.updateCard(cardToEdit);
-        }
+        flashcardDatabase.updateCard(cardToEdit);
+    }
+
+}
+
 
         allFlashcards = flashcardDatabase.getAllCards();
 
